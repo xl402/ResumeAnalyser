c184fc4110cdae67e6a5a5d2e6399c4456116021
Tom Lu <name@seamlessml.com>
2024-02-12 21:47:56 -0800

    refactor test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,11 @@ import os
 import pytest
 
 
-@pytest.fixture
-def resource_dir():
-    repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(repo_path, 'resources')
+repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+RESOURCE_DIR = os.path.join(repo_path, 'resources')
 
 
 @pytest.fixture
-def example_resume_path(resource_dir):
-    path = os.path.join(resource_dir, 'example_resume.pdf')
+def example_resume_path():
+    path = os.path.join(RESOURCE_DIR, 'example_resume.pdf')
     return path
